096f4786cfe68fbb75e43ef88cebc5dd9670bd5c
Blackwell-michael <mblackw1@umbc.edu>
2025-05-12 14:52:50 -0400

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,57 +1,44 @@
 //Younoussa Daffe
 //ydaffe1@umbc.edu
-
-
-/**
-* @param ~ title - Title of the book
-* @param ~ author - Author of the book
-* @param ~ genre - Genre of the book
-* @param ~ numberOfCopies - Number of copies of specific book
-* @param ~ available copies - Number of copies avaiable of specific book
- * @param ~ ISBN # of book
-*/
-    public class Book {
-        private String title;
-        private String author;
-        private String genre;
-        private int numberOfCopies;
-        private int availableCopies;
-        private String ISBN;
+// Refactored core class: Book.java
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int numberOfCopies;
+    private int availableCopies;
+    private String ISBN;
 
     /**
-     * @Method ~ Book -
+     * Constructs a Book object.
+     * @param title Title of the book
+     * @param author Author of the book
+     * @param genre Genre of the book
+     * @param numberOfCopies Total copies of the book
+     * @param ISBN ISBN number of the book
      */
-    public Book (String title, String author, String genre, int numberOfCopies, String ISBN) {
+    public Book(String title, String author, String genre, int numberOfCopies, String ISBN) {
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.numberOfCopies = numberOfCopies;
-        this.availableCopies = availableCopies;
+        this.availableCopies = numberOfCopies;
         this.ISBN = ISBN;
-    }
-    /**
-     * @Method ~ isAvailable -  to see if copy of book is available
-     */
-    public boolean isAvailable() {
-        if (availableCopies > 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
     }
 
     /**
-     * @Method ~ incrementCopies - to increase number of copies
+     * Checks if the book is available to borrow.
+     * @return true if available copies > 0
      */
+    public boolean isAvailable() {
+        return availableCopies > 0;
+    }
+
     public void incrementCopies(int amount) {
         this.numberOfCopies += amount;
         this.availableCopies += amount;
     }
 
-    /**
-     * @Method ~ decrementCopies - to decrease number of copies
-     */
     public void decrementCopies(int amount) {
         this.numberOfCopies -= amount;
         this.availableCopies -= amount;
@@ -61,4 +48,53 @@
     public String getAuthor() { return author; }
     public String getGenre() { return genre; }
     public String getISBN() { return ISBN; }
+}
+
+// Partial refactor for Member.java to fix casting issue
+public class Member {
+    private String memID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Queue<Book> borrowedBooks;
+    public static final int BORROW_LIMIT = 3; // Unified limit
+
+    public Member(String memID, String firstName, String lastName, String email) {
+        this.memID = memID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.borrowedBooks = new LinkedList<>();
+    }
+
+    public String getMemberId() { return memID; }
+    public String getName() { return firstName + " " + lastName; }
+    public String getEmail() { return email; }
+
+    public List<Book> getBorrowedBooks() {
+        return new ArrayList<>(borrowedBooks);
+    }
+
+    public void borrowBook(Book book) throws BorrowLimitExceededException, ItemNotAvailableException {
+        if (borrowedBooks.size() >= BORROW_LIMIT) {
+            throw new BorrowLimitExceededException(BORROW_LIMIT);
+        }
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return "Member ID: " + memID + ", Name: " + getName() + ", Email: " + email +
+                ", Books Borrowed: " + borrowedBooks.size();
+    }
+}
+
+// Constants.java
+public class Constants {
+    public static final int BORROW_LIMIT = 3;
+    public static final String MEMBER_ID_PREFIX = "MEM";
 }
